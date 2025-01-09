6268e5259ea70a73d282aa5a87d84ca125f36ba7
Madina-Web-Developer <developermadina761@gmail.com>
2025-01-09 19:29:08 +0600

    data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import './App.css'
-import Data from './Components/Realtime Database/Data'
+import About from './Components/Pages/About'
+import Home from './Components/Pages/Home'
+import app from './Firebase.config'
 
 
 
 function App() {
+
+
+  const myRoute = createBrowserRouter(
+    createRoutesFromElements(
+
+      <Route>
+
+        <Route index element={<Home/>}/>
+
+        <Route path='/about' element={<About/>}/>
+
+
+      </Route>
+    )
+  )
  
 
   return (
     <>
 
-  <Data/>
+    <RouterProvider router={myRoute}/>
      
     </>
   )
---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Data from '../Realtime Database/Data'
+
+const Home = () => {
+  return (
+    <div>
+
+        <Data/>
+        
+    </div>
+  )
+}
+
+export default Home
